fdbaca2edddf7a01dc78cf183870fc65d4e03466
Emerik <emeriklatour@gmail.com>
2022-02-28 14:34:42 -0500

    Création de JeuTest, JeuBunco et BuncoStrategie. JeuBunco est implémentée afin de tester Jeu plus simplement. Il reste a implémeter le test de Jeu, le test de StrategieBunco et la StrategieBunco pour que le projet soit fini.

---
+++ src/main/java/framework/bunco/StrategieBunco.java
@@ -0,0 +1,16 @@
+package framework.bunco;
+
+import framework.game.IStrategie;
+import framework.game.Jeu;
+
+public class BuncoStrategie implements IStrategie {
+    @Override
+    public void calculerGagnant(Jeu jeu) {
+        return;
+    }
+
+    @Override
+    public void calculerScoreTour(Jeu jeu) {
+        return;
+    }
+}
