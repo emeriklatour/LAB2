f3833b3dc2d543fc86ad8336e48355a8179f20dc
Emerik <emeriklatour@gmail.com>
2022-03-07 21:45:12 -0500

    StrategieBunco est terminée. Modification de tout le projet au complet. (commentaires et affichages.)

--- src/main/java/bunco/StrategieBunco.java
+++ src/main/java/bunco/StrategieBunco.java
@@ -15,6 +15,9 @@ import java.util.*;
  * @date 2022/03/02
  *******************************************************/
 public class StrategieBunco implements IStrategie {
+    int _nbLancerCourant = 0;
+    int _score = 0; //Score total du tour courant pour un joueur.
+
     @Override
     public void calculerGagnant(Jeu jeu) {
         Joueur gagnant = jeu.getAllJoueurs().next();
@@ -33,81 +36,64 @@ public class StrategieBunco implements IStrategie {
 
     @Override
     public void calculerScoreTour(Jeu jeu) {
-        int score = 0; //Score total du tour courant pour un joueur.
-        int dePoints; //Pour savoir le nombre de points à ajouter au score à chaque lancé.
-        int miniBunco; //Pour savoir s'il y a un mini bunco.
-        int nbLancerCourant = 0;
-        boolean lancerEncore = true;
+        int dePoints = 0; //Pour savoir le nombre de points à ajouter au score à chaque lancé.
+        int miniBunco = 0; //Pour savoir s'il y a un mini bunco.
+        int currentJoueur = jeu.getCurrentJoueur();
 
-        System.out.println("***************************************");
-        //Loop qui loop tant et aussi longtemps qu'un joueur gagne des points, sauf s'il gagne un bunco.
-        while (lancerEncore){
-            nbLancerCourant++;
-            dePoints = 0;
-            miniBunco = 0;
-            System.out.print("Joueur " + (jeu.getCurrentJoueur()+1) + " lance " + (nbLancerCourant) + " fois. (");
-            roulerLesDes(jeu.getAllDes());
-            /*
-            J'initialise un Iterator<De> à chaque fois qu'on loop (à chaque lancé)
-            parce que sinon l'index reste à la fin de l'itérateur et le code va passer par-dessus
-            parce que hasNext() retourne false. J'ai mis longtemps à trouver l'erreur.
-             */
-            Iterator<De> des = jeu.getAllDes();
-            De first = jeu.getAllDes().next(); //Pour comparer les dés et savoir s'il y a un mini Bunco. P.S. Toujours égal à 1
-            De currentDe;
-            while(des.hasNext()){
-                currentDe = des.next();
-                if(currentDe.getCurrentFace() == jeu.getCurrentTurnNb())
-                    dePoints++;
-                if(currentDe.compareTo(first) == 0)
-                    miniBunco++;
-            }
-
-            //Compte des points obtenus pour le lancé.
-            if(dePoints == 3) {
-                /*
-                Si dePoints = 3, les 3 dés sont pareils au nombre du tour courant. score = 21 et on sort de la loop
-                parce que le tour est fini.
-                 */
-                score = 21;
-                System.out.println("bunco! 21 points.)");
-                break;
-            } else if (miniBunco == 3){
-                /*
-                Si miniBunco = 3, les 3 dés sont pareils. score + 5 et on lance une autre fois.
-                 */
-                score = score + 5;
-                System.out.println("mini bunco! +5 points.)");
-            } else if (dePoints == 1){
-                /*
-                Si dePoints = 1, une face d'un des dés lancés est égale au nombre du tour courant. On ajoute 1 au score courant et on lance
-                une autre fois.
-                 */
-                score = score + 1;
-                System.out.println(1 + " points.)");
-            } else if (dePoints == 2){
-                /*
-                Si dePoints = 2, 2 face des dés lancés sont égales au nombre du tour courant. On ajoute 2 au
-                score et on lance une autre fois.
-                 */
-                score = score + 2;
-                System.out.println((2 + " points.)"));
-            } else {
-                /*
-                Aucun point n'a été gagné. lancerEncore devient faux, ce qui nous fait sortir de la loop et on passe au prochain joueur.
-                 */
-                lancerEncore = false;
-                System.out.println("0 points.)");
-            }
+        System.out.print("Joueur " + (currentJoueur+1) + " lance " + (_nbLancerCourant+=1) + " fois. (");
+        roulerLesDes(jeu.getAllDes());
+        /*
+        J'initialise un Iterator<De> à chaque à chaque lancé
+        parce que sinon l'index reste à la fin de l'itérateur et le code va passer par-dessus
+        parce que hasNext() retourne false. J'ai mis longtemps à trouver l'erreur.
+         */
+        Iterator<De> des = jeu.getAllDes();
+        De first = jeu.getAllDes().next(); //Pour comparer les dés et savoir s'il y a un mini Bunco. P.S. Toujours égal à 1
+        De currentDe;
+        while(des.hasNext()){
+            currentDe = des.next();
+            if(currentDe.getCurrentFace() == jeu.getCurrentTurnNb())
+                dePoints++;
+            if(currentDe.compareTo(first) == 0)
+                miniBunco++;
         }
 
-        System.out.println("Joueur " + (jeu.getCurrentJoueur()+1) + " a obtenu " + score + " points pour le tour "
-                + jeu.getCurrentTurnNb() + ".");
-        System.out.println("***************************************");
-        System.out.println();
-        //Ajout du score obtenu dans le tour au score du joueur courant.
-        Joueur currentJoueur = getCurrentJoueur(jeu.getCurrentJoueur(), jeu.getAllJoueurs());
-        currentJoueur.setScore((currentJoueur.getScore()+ score));
+        //Compte des points obtenus pour le lancé.
+        if(dePoints == 3) {
+            /*
+            Si dePoints = 3, les 3 dés sont pareils au nombre du tour courant. score = 21 et on passe au prochain joueur.
+             */
+            _score = 21;
+            System.out.println("bunco! 21 points.)");
+            setScoreAndReset(jeu, currentJoueur);
+        } else if (miniBunco == 3){
+            /*
+            Si miniBunco = 3, les 3 dés sont pareils. score + 5 et on lance une autre fois.
+             */
+            _score += 5;
+            System.out.println("mini bunco! +5 points.)");
+        } else if (dePoints == 1){
+            /*
+            Si dePoints = 1, une face d'un des dés lancés est égale au nombre du tour courant. On ajoute 1 au score courant et on lance
+            une autre fois.
+             */
+            _score += 1;
+            System.out.println(1 + " points.)");
+        } else if (dePoints == 2){
+            /*
+            Si dePoints = 2, 2 face des dés lancés sont égales au nombre du tour courant. On ajoute 2 au
+            score et on lance une autre fois.
+             */
+            _score += 2;
+            System.out.println((2 + " points.)"));
+        } else {
+            /*
+            Aucun point n'a été gagné et on passe au prochain joueur.
+             */
+            System.out.println("0 points.)");
+            setScoreAndReset(jeu, currentJoueur);
+        }
+
     }
 
     /**
@@ -119,6 +105,22 @@ public class StrategieBunco implements IStrategie {
             De de = des.next();
             de.roulerDe();
         }
+    }
+
+    /**
+     * Ajoute le score au joueur courant et réinitialise les variables de la classe pour l'affichage du prochain joueur.
+     * @param jeu le jeu Bunco
+     * @param currentJoueur l'index du joueur courrant.
+     */
+    private void setScoreAndReset(Jeu jeu, int currentJoueur){
+        _nbLancerCourant = 0;
+        System.out.println("Joueur " + (currentJoueur+1) + " a obtenu " + _score + " points pour le tour "
+                + jeu.getCurrentTurnNb() + ".");
+        jeu.setCurrentJoueur(currentJoueur+1);
+        //Ajout du score obtenu dans le tour au score du joueur courant.
+        Joueur joueur = getCurrentJoueur(currentJoueur, jeu.getAllJoueurs());
+        joueur.setScore((joueur.getScore()+ _score));
+        _score = 0;
     }
 
     /**
